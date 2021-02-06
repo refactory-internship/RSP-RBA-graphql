37dc6bdb15facd55eaba94b36c399ce1e7464a0e
ffajar.pratama <17523094@students.uii.ac.id>
2021-02-06 17:34:42 +0700

    another update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -17,11 +17,11 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    username: "gpapngxnduncgq",
-    password: "7be57bde318ce625afe31187651af2836324f671eb2b1a1a2a802efc75baa5ac",
-    database: "d93qceuoq8dkof",
-    host: "ec2-52-6-178-202.compute-1.amazonaws.com",
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: "127.0.0.1",
     dialect: "postgres",
-    logging: false
+    use_env_variable: "DATABASE_URL"
   }
 }
