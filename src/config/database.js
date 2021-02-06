e9ac1423ed291c8f3fa6a9312255d99c05efeb87
ffajar.pratama <17523094@students.uii.ac.id>
2021-02-06 17:03:55 +0700

    update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -17,7 +17,7 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    "use_env_variable": "DATABASE_URL",
-    "dialect": "postgres",
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
   }
 }
